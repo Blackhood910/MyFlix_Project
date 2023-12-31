91fb796e8f6b803a5480715b9ffc55332dc4f3f3
Blackhood910 <haseemzafeer19@gmail.com>
2023-12-31 22:44:27 +0000

    added creating profile functionality

--- netflixproj/netflixapp/urls.py
+++ netflixproj/netflixapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, ProfileList
+from .views import Home, ProfileList , ProfileCreate 
 
 app_name = 'netflixapp'
 
@@ -7,4 +7,5 @@ app_name = 'netflixapp'
 urlpatterns = [
     path('',Home.as_view(),name="Home"),
     path('profiles/',ProfileList.as_view(),name="profile-list"),
+    path('profiles/create/',ProfileCreate.as_view(),name="profile-create")
 ]
--- netflixproj/netflixapp/views.py
+++ netflixproj/netflixapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from . forms import ProfileForm
+from . models import Profile
 
 class Home(View):
     def get(self, request, *args , **kwargs):
@@ -18,11 +20,22 @@ class ProfileList(View):
         }
         return render( request ,'profilelist.html', context)
 
-method_decorator(login_required, name='dispatch')     
-class ProfileList(View):
-    def get(self, request, *args , **kwargs):
-        profiles=request.user.profiles.all()
-        context={
-            'profiles' : profiles
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        form = ProfileForm()
+        context = {
+            'form':form
         }
-        return render( request ,'profilelist.html', context)
+        return render(request, 'profilecreate.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+        if form.is_valid():
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('netflixapp:profile-list')
+        context = {
+            'form':form
+        }
+        return render(request, 'profilecreate.html', context)    
