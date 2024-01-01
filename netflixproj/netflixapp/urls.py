812079850902a2c89bf3a18ed81ea81aaf6cab93
Blackhood910 <haseemzafeer19@gmail.com>
2024-01-01 19:59:33 +0000

    adding movie list functions in the project

--- netflixproj/netflixapp/urls.py
+++ netflixproj/netflixapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, ProfileList , ProfileCreate 
+from .views import Home, ProfileList , ProfileCreate  , MovieList
 
 app_name = 'netflixapp'
 
@@ -7,5 +7,6 @@ app_name = 'netflixapp'
 urlpatterns = [
     path('',Home.as_view(),name="Home"),
     path('profiles/',ProfileList.as_view(),name="profile-list"),
-    path('profiles/create/',ProfileCreate.as_view(),name="profile-create")
+    path('profiles/create/',ProfileCreate.as_view(),name="profile-create"),
+    path('watch/<str:profile_id>/',MovieList.as_view(),name="movie-list"),
 ]
