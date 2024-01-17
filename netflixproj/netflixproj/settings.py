32ee172cc23565001f6a4689ab69cb06a4f63601
Blackhood910 <haseemzafeer19@gmail.com>
2024-01-17 23:15:19 +0000

    adding enviroment code and setting .py

--- netflixproj/netflixproj/settings.py
+++ netflixproj/netflixproj/settings.py
@@ -87,15 +87,16 @@ WSGI_APPLICATION = 'netflixproj.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mfdatabase',
-        'USER': 'blackhood910',
-        'PASSWORD': 'Hinata135',
-        'HOST': 'mfdatabase.cr0emk8ooxaj.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE"),
+        "NAME": os.environ.get("SQL_DATABASE"),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
     }
 }
+
 
 
 # Password validation
